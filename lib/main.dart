8cb33bf48fee9f5db36db43568571c9a2730ea05
imobasshir <iibrahimimam24@gmail.com>
2021-11-13 02:29:41 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,38 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s yout favorate color?',
+      'What\'s your favorate animal?',
+    ];
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text("My First App"),
       ),
-      body: Text("Hello Mobasshir Imam!"),
+      body: Column(
+        children: [
+          Text('The question!'),
+          ElevatedButton(
+            onPressed: answerQuestion,
+            child: Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: answerQuestion,
+            child: Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: answerQuestion,
+            child: Text('Answer 3'),
+          ),
+        ],
+      ),
     ));
   }
 }
