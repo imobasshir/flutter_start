7c875426e2765cd44ba9a092edca63bc197d5b5b
imobasshir <iibrahimimam24@gmail.com>
2021-11-14 01:24:45 +0530

    sixth commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,18 +8,18 @@ class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
-  void answerQuestion() {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
     setState(() {
-      questionIndex += 1;
+      _questionIndex += 1;
     });
 
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -35,9 +35,9 @@ class MyAppState extends State<MyApp> {
       ),
       body: Column(
         children: [
-          Text(questions[questionIndex]),
+          Text(questions[_questionIndex]),
           ElevatedButton(
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
             child: Text('Answer 1'),
           ),
           ElevatedButton(
